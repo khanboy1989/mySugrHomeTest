efd44734f4cb69cd83bb2cafde98cdd687aedfd8
Serhan Khan <khanboy0689@gmail.com>
2024-12-11 17:53:46 +0200

    gitIgnoreFile Added

---
+++ mySugrHomeTest/mySugrHomeTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  mySugrHomeTestApp.swift
+//  mySugrHomeTest
+//
+//  Created by Serhan Khan on 11/12/2024.
+//
+
+import SwiftUI
+
+@main
+struct mySugrHomeTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
